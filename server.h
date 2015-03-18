f0bd6bf35a73bf170b63230a3b48a1b906505e1f
Joshua Comish <com09003@byui.edu>
2015-03-17 21:47:17 -0600

    Added client functionality

--- server.h
+++ server.h
@@ -10,14 +10,15 @@
 
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <string> 
+#include <string.h>
 #include <stdlib.h> 
 #include <stdio.h> 
 #include <netinet/in.h> 
-#include "iostream"
+#include <iostream>
 #include "move.h"
-#include<unistd.h>
-#include<arpa/inet.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <netdb.h>
 using namespace std;
 
 class Server 
@@ -32,13 +33,10 @@ class Server
     /* const int OP_MOVE3 = 3; */
     /* const int OP_MOVE4 = 4; */
     
-    static bool findClient()
+    static void server()
     {
         cout << "What port number will you use? (must be greater than 1000) : ";
         cin >> portNum;
-        
-        
-        cout << "Enter other player's port number: ";
         
         int socketServer, player1, player2, clilen1, clilen2;
         char buffer[1];
@@ -60,7 +58,7 @@ class Server
         if (::bind(socketServer, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
         {
             cerr << "ERROR on BINDING\n";
-            return false;
+            return;
         }
         
         bzero(buffer,1);
@@ -77,10 +75,10 @@ class Server
         if (player1 < 0)
         {
             cerr << "player1 failed to accept";
-            return false;
+            return;
         }
         
-        cout << "looking for 2nd client\n";
+        cout << "SUCCESS!!\n";
         clilen2 = sizeof(p2_addr);
         
         player2 = accept(socketServer, 0, 0);
@@ -89,12 +87,79 @@ class Server
         if (player2 < 0)
         {
             cerr << "player2 failed to accept";
-            return false;
+            return;
+        }
+
+    }
+    
+    static void client()
+    {
+        string hostName;
+        cout << "Enter other player's hostname: ";
+        cin >> hostName;
+        
+        int targetPort;
+        cout << "Enter other player's port number: ";
+        cin >> targetPort;
+        
+        
+        int sockfd, portno, n;
+        struct sockaddr_in serv_addr;
+        struct hostent *server;
+        
+        char buffer[1];
+        //if (argc < 3) {
+        //    fprintf(stderr,"Usage: %s (hostname) (port) e.g.: client aus213l19 5000\n", argv[0]);
+        //    exit(0);
+        //}
+        
+        //connect to the server
+        portno = targetPort;
+        sockfd = socket(AF_INET, SOCK_STREAM, 0);
+        //if (sockfd < 0)
+        //    error("ERROR opening socket");
+        
+        server = gethostbyname(hostName.c_str());
+        
+        if (server == NULL) {
+            fprintf(stderr,"ERROR, no such host\n");
+            exit(0);
         }
         
-        return true;
-
+        bzero((char *) &serv_addr, sizeof(serv_addr));
+        serv_addr.sin_family = AF_INET;
+        bcopy((char *)server->h_addr,
+              (char *)&serv_addr.sin_addr.s_addr,
+              server->h_length);
+        serv_addr.sin_port = htons(portno);
         
+        if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0)
+            //error("ERROR connecting");
+        
+        if (read(sockfd, buffer, 1))
+            //listen for input from the server
+            /*while(buffer[0] != 'q')
+            {
+                printf("Please enter your choice (r,p,s,q): ");
+                bzero(buffer,1);
+                cin >> buffer;
+                
+                translateSend(buffer, sockfd);
+                
+                bzero(buffer,1);
+                translateRead(buffer, sockfd);
+                
+            }*/
+        close(sockfd);
+        
+        return;
+
+    }
+    
+    static bool findClient()
+    {
+        server();
+        client();
         
         return true;
     }
